85e9fe9406e1ee6a70bc241dc7b6f8eec43ca118
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-10-12 19:32:22 -0500

    more changes to the seed file

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,8 +2,8 @@ const { Schema, Types } = require('mongoose')
 
 const reactionSchema = new Schema ({
   reactionId: {
-    type: Schema.ObjectId,
-    default: () => new ObjectId()
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
   },
   reactionBody: {
     type: String,
--- utils/seed.js
+++ utils/seed.js
@@ -6,20 +6,22 @@ const thoughtData = require("./thoughtData.json")
 
 connection.once("open", async () => {
     try {
-        await User.deleteMany({})
-        await Thought.deleteMany({})
+        // await User.deleteMany({})
+        // await Thought.deleteMany({})
 
-        const users = await User.create(userData)
+        await User.collection.insertMany(userData)
+        await Thought.collection.insertMany(thoughtData)
+
         
-        for (const thought of thoughtData) {
-            const randomIndex = Math.floor(Math.random * users.length)
-            const randomUser = users[randomIndex]
+        // for (const thought of thoughtData) {
+        //     const randomIndex = Math.floor(Math.random * users.length)
+        //     const randomUser = users[randomIndex]
 
-            const thoughtWithUsername = {...thought, username: randomUser.username}
-            const newThought = await Thought.create(thoughtWithUsername)
+        //     const thoughtWithUsername = {...thought, username: randomUser.username}
+        //     const newThought = await Thought.create(thoughtWithUsername)
 
-            await User.findbyIDAndUpdate(randomUser._id, {$push: {thoughts: newThought._id }})
-        }
+        //     await User.findbyIDAndUpdate(randomUser._id, {$push: {thoughts: newThought._id }})
+        // }
     
         process.exit(0)
     } catch(err) {
